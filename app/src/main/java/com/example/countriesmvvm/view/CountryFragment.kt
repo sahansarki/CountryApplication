0736eec1101ec61fef8c35c0df234d87abf9e5e8
sahan <sahancansarki@gmail.com>
2021-05-13 19:23:57 +0300

    Country Application First Comment

---
+++ app/src/main/java/com/example/countriesmvvm/view/CountryFragment.kt
@@ -0,0 +1,31 @@
+package com.example.countriesmvvm.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.countriesmvvm.R
+
+
+class CountryFragment : Fragment() {
+
+    private var countryUuid = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_country, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+            countryUuid = CountryFragmentArgs.fromBundle(it).countryUuid
+            println(countryUuid)
+        }
+    }
+
+}
